90c67916ccc59fd70870e50cba3ee9d2f380b7cb
Harsh Nagpal <harshnagpal2502@gmail.com>
2022-08-27 00:44:58 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,11 @@
-const http = require('http').createServer();
+const http = require('http').createServer(); //Creates a Server
 
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
-});
+}); //Server Socket Initialization
 
 io.on('connection', (socket) => {
     console.log('User Connected');
-
     socket.on('message', (message) =>     {
         console.log(message);
         io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
